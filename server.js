0addd920f944487b55a58c251456cf228ad710a9
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2025-05-07 21:12:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post('/generate', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
