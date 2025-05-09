27e776d86e4fa433e0970d871bb493b022e0c2a0
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2025-05-09 01:12:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -35,5 +35,8 @@ app.post('/generate-mindmap', async (req, res) => {
   }
 });
 
+const cors = require('cors');
+app.use(cors({ origin: 'https://JavierCLT.github.io' }));
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
