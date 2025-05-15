8440f31f37c399ba820d57df7f709210418a8b97
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2025-05-14 22:52:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.json())
 // Configure rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 30, // limit each IP to 30 requests per windowMs
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: "Too many requests from this IP, please try again after 15 minutes"
